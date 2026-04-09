956783d435545bdcea395615e57e45990b2eb3f1
Static-4eb7cf82 <static.4eb7cf82@gmail.com>
2026-04-08 23:50:51 -0700

    use window label for routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,10 +6,6 @@ import { useMemo } from "react";
 
 function App() {
   const windowLabel = useMemo(() => {
-    const fromQuery = new URLSearchParams(window.location.search).get("window");
-    if (fromQuery) {
-      return fromQuery;
-    }
     try {
       return getCurrentWindow().label;
     } catch {
